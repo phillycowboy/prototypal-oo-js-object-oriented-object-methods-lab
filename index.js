06bd29e103afb8d45bb4425d59cb91d1b9f0231f
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-05-06 16:11:30 -0400

    commit tree 387e798546944b6fb50904bd7a82a2c929f199b8

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-class BoardMember{
-    constructor(name, homeState, training){
+function BoardMember(name, homeState, training){
         this.name = name;
         this.homeState = homeState;
         this.training = training;
     }
 
-    veto(){
-        return "No, I must disagree";
-    }
-    approve(){
-        return "You can do that!";
-    }
-    doCharity(){
-        return "I like to help people."
-    }
-    releasePressStatement(){
-        return "You will see great things from Scuber."
-    }
-    sayHi(){
-        return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-    }
-};
+    // veto(){
+    //     return "No, I must disagree";
+    // }
+    // approve(){
+    //     return "You can do that!";
+    // }
+    // doCharity(){
+    //     return "I like to help people."
+    // }
+    // releasePressStatement(){
+    //     return "You will see great things from Scuber."
+    // }
+    // sayHi(){
+    //     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+    // }
+
+BoardMember.prototype.veto = () => "No, I must disagree";
+BoardMember.prototype.approve = () => "You can do that!";
+BoardMember.prototype.doCharity = () => "I like to help people.";
+BoardMember.prototype.releasePressStatement = () => "You will see great things from Scuber.";
+BoardMember.prototype.sayHi = function(){
+ return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
+}    
+// let sayHi = function() {
+//     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
+// }
